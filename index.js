8f06a6d31e544a52c949d29e0e8258b6ff0c410c
Jessica Amos <amosjessicar@gmail.com>
2023-12-18 12:13:39 -0500

    redo

--- index.js
+++ index.js
@@ -1,57 +1,86 @@
 // Programmer: Jessica Amos
 // Purpose: Create a terminal-based calculator using Node
 
-var rs = require('readline-sync');
+const rs = require('readline-sync');
 
+const arrOp = ['/','*','-','+'];
 
-function isOperator(value) {
-  let operators = new Set(['/','*','-','+']);
-  return operators.has(value);
-}
-
-function isNumber(num){
-  if (isNaN(num)){
-    console.log(`${num} is not a number. `);
-    firstNumber = rs.question('Please enter a number. ');
+const getOperation = (limit) => {
+  while(true) {
+    const operation = rs.question('What operation would you like to perform? ');
+    if (limit.includes(operation)){
+      return operation;
+    } else {
+      console.log(`${operation} is not a valid operation. `);
+    }
   }
-  return parseFloat(num);
 }
 
-function evaluate(num1, num2, operator){
-  switch (operator){
-    case '+': 
-      result = num1 + num2;
-      break;
-    case '-': 
-      result = num1 - num2;
-      break;
-    case '/': 
-      result = num1 / num2;
-      break;
-    case '*': 
-      result = num1 * num2;
-      break;
-    default:
-      console.log('Invalid operator');
-      return
+const getNumber = (str) => {
+  const number = rs.questionInt(`Please enter the ${str} number. `);
+  if (!isNaN(number) && number != '') {
+    return +number
+  } else {
+    console.log('This is not a valid number.')
   }
-  return console.log(`The result is: ${result} `)
 }
 
-let operation = rs.question('What operation would you like to perform? ');
-isOperator(operation);
+const operation = getOperation(arrOp);
+const num1 = getNumber('first');
+const num2 = getNumber('second');
 
-while (!isOperator(operation)){
-  console.log(`${operation} is not a valid operation. `);
-  operation = rs.question('What operation would you like to perform? ');
-}
+console.log(num1, operation, num2);
 
-let firstNumber = rs.question('Please enter the first number. ');
 
-isNumber(firstNumber);
 
-let secondNumber = rs.question('Please enter the second number. ');
+// function isOperator(value) {
+//   let operators = new Set(['/','*','-','+']);
+//   return operators.has(value);
+// }
 
-isNumber(secondNumber);
+// function isNumber(num){
+//   if (isNaN(num)){
+//     console.log(`${num} is not a number. `);
+//     firstNumber = rs.question('Please enter a number. ');
+//   }
+//   return parseFloat(num);
+// }
 
-evaluate(firstNumber, secondNumber, operation);
+// function evaluate(num1, num2, operator){
+//   switch (operator){
+//     case '+': 
+//       result = num1 + num2;
+//       break;
+//     case '-': 
+//       result = num1 - num2;
+//       break;
+//     case '/': 
+//       result = num1 / num2;
+//       break;
+//     case '*': 
+//       result = num1 * num2;
+//       break;
+//     default:
+//       console.log('Invalid operator');
+//       return
+//   }
+//   return console.log(`The result is: ${result} `)
+// }
+
+// let operation = rs.question('What operation would you like to perform? ');
+// isOperator(operation);
+
+// while (!isOperator(operation)){
+//   console.log(`${operation} is not a valid operation. `);
+//   operation = rs.question('What operation would you like to perform? ');
+// }
+
+// let firstNumber = rs.question('Please enter the first number. ');
+
+// isNumber(firstNumber);
+
+// let secondNumber = rs.question('Please enter the second number. ');
+
+// isNumber(secondNumber);
+
+// evaluate(firstNumber, secondNumber, operation);
